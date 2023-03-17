44418fed82a015943a269a191cec3f3ddcb1d295
Shashank Reddy <shashank.godala.com>
2023-03-17 19:22:38 +0000

    first commit

---
+++ src/components/EmojiCard/index.js
@@ -0,0 +1,14 @@
+// Write your code here.
+import {Component} from 'react'
+
+import NavBar from '../NavBar'
+import EmojiGame from '../EmojiGame'
+
+import './index.css'
+
+
+import EmojiCard extends Component {
+
+}
+
+export default EmojiCard
